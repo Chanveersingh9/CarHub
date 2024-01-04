07b4e202da039b9a4a24ba9e77069ba6f693a37f
Chanveersingh9 <141109211+Chanveersingh9@users.noreply.github.com>
2024-01-04 10:40:01 -0500

    Add files via upload

---
+++ PB.js
@@ -0,0 +1,7 @@
+document.querySelector('button.language').addEventListener('click', function() {
+    window.location.href = 'https://www.example.com/language';
+});
+
+document.querySelector('button.signin').addEventListener('click', function() {
+    window.location.href = 'https://www.example.com/signin';
+});
